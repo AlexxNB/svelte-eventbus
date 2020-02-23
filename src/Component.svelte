221579515706817eb21921b83853e299634aa459
AlexxNB <iam@alexxnb.ru>
2020-02-23 22:41:48 +0300

    initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+    import { createEventDispatcher,setContext, getContext } from 'svelte';
+
+    export function initEventbus(){
+        setContext("SVELTE_EVENTBUS_DISPATCHER",createEventDispatcher());
+    }
+
+    export function createEventbusDispatcher(){
+        return getContext("SVELTE_EVENTBUS_DISPATCHER");
+    }
+</script>
+
+<script>
+	initEventbus();
+</script>
+
+<slot/>
